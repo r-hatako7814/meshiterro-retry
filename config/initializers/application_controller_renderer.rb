492c6aaaef572a8a92a8801c504fa528f847f53d
r-hatako7814 <ryuichiro.hata@gmail.com>
2021-10-08 18:08:03 +0000

    10/8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '2a4a504beb53aea26292bf4329610345ddb06672a020afd7269fb7f11366ae0c207bbb9056e03a4a7c512d934757a82ba9116f5a6a42f26225e257f098c959ac'
