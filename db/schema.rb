3ef79292c2a3c3c92a91722a923f3b95d986fc02
r-hatako7814 <ryuichiro.hata@gmail.com>
2021-10-09 16:42:51 +0000

    10/9

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_07_094004) do
+ActiveRecord::Schema.define(version: 2021_10_09_163613) do
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "iser_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_images", force: :cascade do |t|
     t.text "shop_name"
