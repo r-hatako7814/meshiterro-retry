5d146291ca1c41fd0c7ffbf0bbedd6607f7b08dd
r-hatako7814 <ryuichiro.hata@gmail.com>
2021-10-10 14:54:00 +0000

    10/10

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'post_comment/create'
+  get 'post_comment/destroy'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
