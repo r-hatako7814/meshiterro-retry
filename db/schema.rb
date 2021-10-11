037214715abbc29337c8d0eeb98e3bc30580073f
r-hatako7814 <ryuichiro.hata@gmail.com>
2021-10-11 12:44:37 +0000

    10/11

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_09_163613) do
+ActiveRecord::Schema.define(version: 2021_10_11_090208) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_comments", force: :cascade do |t|
     t.text "comment"
-    t.integer "iser_id"
+    t.integer "user_id"
     t.integer "post_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
